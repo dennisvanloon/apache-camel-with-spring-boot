39cf7d4860ace15b5ae7ac861b33dfa05b9d0b39
Dennis van Loon <>
2021-11-30 22:04:11 +0100

    Finish section 1, add file router

--- camel-microservice-a/src/main/java/com/microservices/camelmicroservicea/routes/a/MyFirstTimerRouter.java
+++ camel-microservice-a/src/main/java/com/microservices/camelmicroservicea/routes/a/MyFirstTimerRouter.java
@@ -2,11 +2,22 @@ package com.microservices.camelmicroservicea.routes.a;
 
 import java.time.LocalDateTime;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class MyFirstTimerRouter extends RouteBuilder {
+
+    @Autowired
+    GetCurrentTimeBean getCurrentTimeBean;
+
+    @Autowired
+    SimpleLoggingProcessingComponent simpleLoggingProcessingComponent;
 
     @Override
     public void configure() throws Exception {
@@ -14,8 +25,45 @@ public class MyFirstTimerRouter extends RouteBuilder {
         // transformation
         // log
         from("timer:first-timer")
-        .transform().constant("Time is now " + LocalDateTime.now())
+        .log("${body}")
+        .transform().constant("Custom Message")
+        .log("${body}")
+        //beans with a return type actually change the body of the message (transformation)
+        .bean(getCurrentTimeBean, "getCurrentTime")
+        .log("${body}")
+        //beans returning void do not change the body (processing)
+        .bean(simpleLoggingProcessingComponent, "process")
+        .log("${body}")
+        // processors also only process (no transformation)
+        .process(new SimpleLoggingProcessor())
         .to("log:first-timer");
     }
     
+}
+
+@Component
+class GetCurrentTimeBean {
+    public String getCurrentTime() {
+        return "Time is now " + LocalDateTime.now();
+    }
+}
+
+@Component
+class SimpleLoggingProcessingComponent {
+
+    private Logger logger = LoggerFactory.getLogger(SimpleLoggingProcessingComponent.class);
+    
+    public void process(String message) {
+        logger.info("Time for logger in SimpleLoggingProcessingComponent is now {}", LocalDateTime.now());
+    }
+}
+
+class SimpleLoggingProcessor implements Processor {
+
+    private Logger logger = LoggerFactory.getLogger(SimpleLoggingProcessor.class);
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        logger.info("SimpleLoggingProcessor {}", exchange.getMessage().getBody());
+    }
 }
---
+++ camel-microservice-a/src/main/java/com/microservices/camelmicroservicea/routes/b/MyFileRouter.java
@@ -0,0 +1,17 @@
+package com.microservices.camelmicroservicea.routes.b;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyFileRouter extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from("file:files/input?delete=true")
+        .log("${body}")
+        .to("file:files/output");
+        
+    }
+    
+}
