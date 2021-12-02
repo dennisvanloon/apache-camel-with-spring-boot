4645729ddb9c50bc6fef3f639d5daa894fa25b36
Dennis van Loon <>
2021-12-02 17:42:47 +0100

    Using rest services

--- camel-microservice-b/src/main/java/com/microservices/camelmicroserviceb/routes/KafkaReceiverRouter.java
+++ camel-microservice-b/src/main/java/com/microservices/camelmicroserviceb/routes/KafkaReceiverRouter.java
@@ -3,7 +3,7 @@ package com.microservices.camelmicroserviceb.routes;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class KafkaReceiverRouter extends RouteBuilder {
 
     @Override
